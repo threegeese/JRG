95a6f93b799d9693754e71a2422115161d2634cb
threegeese <19970913qjh@gmail.com>
2019-02-11 23:22:41 +0800

    update

---
+++ 012/script.js
@@ -0,0 +1,9 @@
+portfolio1.onclick = function() {
+    portfolioBar.className = 'bar state-1'
+}
+portfolio2.onclick = function() {
+    portfolioBar.className = 'bar state-2'
+}
+portfolio3.onclick = function() {
+    portfolioBar.className = 'bar state-3'
+}
