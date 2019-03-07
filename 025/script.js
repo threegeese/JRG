d57767d21cd3ebab407077e570da7761963d3ca5
threegeese <19970913qjh@gmail.com>
2019-03-07 20:04:50 +0800

    add menuslide

--- 025/script.js
+++ 025/script.js
@@ -7,3 +7,7 @@ portfolio2.onclick = function() {
 portfolio3.onclick = function() {
     portfolioBar.className = 'bar state-3'
 }
+
+window.onscroll = function(e){
+    window.scrollY > 25 ? topNavBar.classList.add("sticky") : topNavBar.classList.remove("sticky");
+}
