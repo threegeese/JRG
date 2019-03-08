10f0984a4211799adc0881d29c540a8ba5f9b06d
threegeese <19970913qjh@gmail.com>
2019-03-08 14:29:29 +0800

    menu

--- 025/script.js
+++ 025/script.js
@@ -11,3 +11,14 @@ portfolio3.onclick = function() {
 window.onscroll = function(e){
     window.scrollY > 25 ? topNavBar.classList.add("sticky") : topNavBar.classList.remove("sticky");
 }
+
+let litags = document.querySelectorAll("nav > ul > li");
+// let litags = document.getElementsByClassName("menuTigger");
+for(let i=0; i<litags.length; i++){
+    litags[i].onmouseenter = function(e) {
+       e.currentTarget.classList.add("active");
+    }
+    litags[i].onmouseleave = function(e) {
+        e.currentTarget.classList.remove("active");
+    }
+}
