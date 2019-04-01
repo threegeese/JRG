c7a9c5945b392f0db9870a9d554bba76c4f7a6e1
threegeese <19970913qjh@gmail.com>
2019-04-01 20:48:36 +0800

    third version

--- 031/main.js
+++ 031/main.js
@@ -1,21 +1,31 @@
 init();
 
 let n = 1;
-setInterval(() => {
-    $(`.images > img:nth-child(${n}`).addClass("dequeue").removeClass("display")
-    .one("transitionend", (e) => {
-        $(e.currentTarget).addClass("queue").removeClass("dequeue");
-    });
-    if(n === 5) {
-        n = 0;
-    }
-    $(`.images > img:nth-child(${n+1}`).removeClass("queue").addClass("display");
-    n += 1;
-},2000)
+let timer = play();
+$(".window").on("mouseenter",() => {
+    clearInterval(timer);
+});
 
+$(".window").on("mouseleave",() => {
+    timer = play();
+});
 
 
 function init() {
     $(".images > img:nth-child(1)").addClass("display");
     $(".images > img:nth-child(1)").siblings().addClass("queue");
+}
+
+function play() {
+    return setInterval(() => {
+        $(`.images > img:nth-child(${n}`).addClass("dequeue").removeClass("display")
+        .one("transitionend", (e) => {
+            $(e.currentTarget).addClass("queue").removeClass("dequeue")
+        });
+        if(n === 5) {
+            n = 0;
+        }
+        $(`.images > img:nth-child(${n+1}`).removeClass("queue").addClass("display");
+        n += 1;
+    },2000)
 }
