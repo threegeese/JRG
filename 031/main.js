1aca281bbcacce250790310308563a55dd8d2bb9
threegeese <19970913qjh@gmail.com>
2019-04-01 20:29:57 +0800

    :bug:

--- 031/main.js
+++ 031/main.js
@@ -1,7 +1,6 @@
-let n = 1;
-$(".images > img:nth-child(1)").addClass("display");
-$(".images > img:nth-child(2)").addClass("queue");
+init();
 
+let n = 1;
 setInterval(() => {
     $(`.images > img:nth-child(${n}`).addClass("dequeue").removeClass("display")
     .one("transitionend", (e) => {
@@ -13,3 +12,10 @@ setInterval(() => {
     $(`.images > img:nth-child(${n+1}`).removeClass("queue").addClass("display");
     n += 1;
 },2000)
+
+
+
+function init() {
+    $(".images > img:nth-child(1)").addClass("display");
+    $(".images > img:nth-child(1)").siblings().addClass("queue");
+}
