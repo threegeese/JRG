3610c70a8010e48ec73fd4549e5da2e67a1ce121
threegeese <19970913qjh@gmail.com>
2019-04-01 20:08:06 +0800

    second version

--- 031/main.js
+++ 031/main.js
@@ -1,47 +1,7 @@
+let n = 1;
 $(".images > img:nth-child(1)").addClass("display");
 $(".images > img:nth-child(2)").addClass("queue");
 
-/*setTimeout(() => {
-    $(".images > img:nth-child(1)").addClass("dequeue").removeClass("display")
-    .one("transitionend", (e) => {
-        $(e.currentTarget).addClass("queue").removeClass("dequeue");
-    });
-    $(".images > img:nth-child(2)").removeClass("queue").addClass("display");
-},2000)
-
-setTimeout(() => {
-    $(".images > img:nth-child(2)").addClass("dequeue").removeClass("display")
-    .one("transitionend", (e) => {
-        $(e.currentTarget).addClass("queue").removeClass("dequeue");
-    });
-    $(".images > img:nth-child(3)").removeClass("queue").addClass("display");
-},4000)
-
-setTimeout(() => {
-    $(".images > img:nth-child(3)").addClass("dequeue").removeClass("display")
-    .one("transitionend", (e) => {
-        $(e.currentTarget).addClass("queue").removeClass("dequeue");
-    });
-    $(".images > img:nth-child(4)").removeClass("queue").addClass("display");
-},6000)
-
-setTimeout(() => {
-    $(".images > img:nth-child(4)").addClass("dequeue").removeClass("display")
-    .one("transitionend", (e) => {
-        $(e.currentTarget).addClass("queue").removeClass("dequeue");
-    });
-    $(".images > img:nth-child(5)").removeClass("queue").addClass("display");
-},8000)
-
-setTimeout(() => {
-    $(".images > img:nth-child(5)").addClass("dequeue").removeClass("display")
-    .one("transitionend", (e) => {
-        $(e.currentTarget).addClass("queue").removeClass("dequeue");
-    });
-    $(".images > img:nth-child(1)").removeClass("queue").addClass("display");
-},10000)
-*/
-let n = 1;
 setInterval(() => {
     $(`.images > img:nth-child(${n}`).addClass("dequeue").removeClass("display")
     .one("transitionend", (e) => {
