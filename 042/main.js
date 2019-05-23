df95402ea089015b0df57375b1f966893ffbcac2
threegeese <19970913qjh@gmail.com>
2019-05-23 14:28:28 +0800

    test

--- 042/main.js
+++ 042/main.js
@@ -20,10 +20,14 @@ main {
     position: relative;
 }
 #prompt {
+    overflow: auto;
     border: 1px solid #aaa;
     margin: 8px;
     padding: 12px;
-    overflow: auto;
+    height: 96vh;
+    width: 48vw;
+    position: absolute;
+    letf: 0;
 }
 
 /* 让代码高亮起来吧 */
@@ -48,60 +52,52 @@ main {
 /* 嗯~ o(*￣▽￣*)o，我需要一个白板 &#128466; */
 `;
 
-let n = 0;
-let prompt = document.getElementById("prompt");
-let addStyle = document.getElementById("addStyle");
-let timer = setInterval(() => {
+const intro = `#board {
+    position: absolute;
+    right: 0;
+    width: 48vw;
+    height: 96vh;
+    background: white;
+    border: 10px solid black;
+    animation: 1.2s board;
+    margin: 8px;
+    padding: 12px;
+}`;
 
-    if(n >= prologue.length - 1) {
-        clearInterval(timer);
-        addBoard();
-        showBoard();
-    }
-    n += 1;
-    prompt.innerHTML = prologue.substring(0, n);
-    prompt.innerHTML = Prism.highlight(prompt.innerHTML, Prism.languages.css);
-    prompt.scrollTop = prompt.scrollHeight;
-    addStyle.innerHTML = prologue.substring(0, n);
-}, 10);
 
-function addBoard() {
+
+writeCode('', prologue, () => {
+    addBoard(() => {
+        writeCode(prologue, intro);
+    });
+});
+
+
+
+function writeCode(prefix, code, fn) {
+
+    let n = 0;
+    let prompt = document.getElementById("prompt");
+    let addStyle = document.getElementById("addStyle");
+    prompt.innerHTML = prefix || '';
+
+    let timer = setInterval(() => {
+        if(n >= code.length) {
+            clearInterval(timer);
+            fn && fn.call();
+        }
+        n += 1;
+        prompt.innerHTML = Prism.highlight(prefix + code.substring(0, n), Prism.languages.css);
+        prompt.scrollTop = prompt.scrollHeight;
+        addStyle.innerHTML = prefix + code.substring(0, n);
+    }, 10);
+
+}
+
+function addBoard(fn) {
     let board = document.createElement("pre");
     board.setAttribute("id","board");
     document.getElementsByTagName("main")[0].appendChild(board);
-}
-
-function showBoard(prePrompt) {
-
-    const intro = `#prompt {
-    width: 48vw;
-    height: 96vh;
-    position: absolute;
-    left: 0;
-}
-#board {
-    width: 48vw;
-    height: 96vh;
-    margin: 8px;
-    padding: 12px;
-    background: white;
-    border: 12px solid black;
-    position: absolute;
-    right: 0;
-}`;
-
-    let n = 0;
-    let timer = setInterval(() => {
-
-        if(n >= intro.length - 1) {
-            clearInterval(timer);
-        }
-        n += 1;
-        prompt.innerHTML = prologue + intro.substring(0, n);
-        prompt.innerHTML = Prism.highlight(prompt.innerHTML, Prism.languages.css);
-        prompt.scrollTop = prompt.scrollHeight;
-        addStyle.innerHTML = prologue + intro.substring(0, n);
-    }, 10);
-    
+    fn.call();
 }
 
