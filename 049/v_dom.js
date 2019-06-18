9f6406d5d2e9b4331fc4c5dab7f65b42789c4972
threegeese <19970913qjh@gmail.com>
2019-06-18 13:39:23 +0800

    diff to-do

--- 049/v_dom.js
+++ 049/v_dom.js
@@ -9,7 +9,7 @@ function VNODE(tagName, props, children) {
     this.children = children;
 }
 
-// 虚拟节点的 render 方法，将虚拟节点构建为DOM
+// 虚拟节点的 render 方法，将虚拟节点构建为DOM节点
 VNODE.prototype.render = function() {
     let el = document.createElement(this.tagName);
     let props = this.props;
@@ -21,12 +21,29 @@ VNODE.prototype.render = function() {
 
     // 处理子节点
     let children = this.children || [];
-    children.forEach(childNode => {
-        childNode instanceof Element ? childNode.render() : document.createTextNode(childNode);
+    children.forEach(child => {
+        let childNode = (child instanceof VNODE)
+                ? child.render()
+                : document.createTextNode(child);
         el.appendChild(childNode); 
     });
 
     return el;
 }
 
+function v(tagName, props, children) {
+    return new VNODE(tagName, props, children);
+}
+
+let ul = new VNODE("ul", { id: "list" }, [
+    v("li", { class: "item" }, ["Item 1"]),
+    v("li", { class: "item" }, ["Item 2"]),
+    v("li", { class: "item" }, ["Item 3"])
+]);
+
+document.body.appendChild(ul.render());
+
 // 2. 比较两棵虚拟DOM树的差异
+function diff(oldTree, newTree) {
+    
+}
